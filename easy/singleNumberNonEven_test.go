3de3fb19ac708b80a7fb48c47a0380d559c5b283
vlaship <vlaship@outlook.com>
2023-03-11 21:03:30 -0500

    added singleNumber

---
+++ easy/singleNumberNonEven_test.go
@@ -0,0 +1,38 @@
+package easy
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_singleNumberNonOnlyEven_1(t *testing.T) {
+	expected := 1
+	result := singleNumberNonOnlyEven([]int{2, 2, 1})
+	if !assert.Equal(t, result, expected) {
+		t.Errorf("Expected %v but got %v", expected, result)
+	}
+}
+
+func Test_singleNumberNonOnlyEven_2(t *testing.T) {
+	expected := 4
+	result := singleNumberNonOnlyEven([]int{4, 1, 2, 1, 2})
+	if !assert.Equal(t, result, expected) {
+		t.Errorf("Expected %v but got %v", expected, result)
+	}
+}
+
+func Test_singleNumberNonOnlyEven_3(t *testing.T) {
+	expected := 1
+	result := singleNumberNonOnlyEven([]int{1})
+	if !assert.Equal(t, result, expected) {
+		t.Errorf("Expected %v but got %v", expected, result)
+	}
+}
+
+func Test_singleNumberNonOnlyEven_4(t *testing.T) {
+	expected := 1
+	result := singleNumberNonOnlyEven([]int{2, 2, 1, 2})
+	if !assert.Equal(t, result, expected) {
+		t.Errorf("Expected %v but got %v", expected, result)
+	}
+}
