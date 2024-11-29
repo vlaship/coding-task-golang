64a75886914cbb2d792289d30bc66681293afe74
vlaship <vlaship@outlook.com>
2024-11-29 00:20:30 -0500

    Refactor lengthOfLongestSubstring function for clarity and efficiency
    Simplify the logic inside the `lengthOfLongestSubstring` function by consolidating variable declarations, removing unnecessary conditions, and streamlining the loop structure. Enhance readability by improving variable names and eliminating redundant code paths.

--- leetcode/medium/problem0003/lengthOfLongestSubstring.go
+++ leetcode/medium/problem0003/lengthOfLongestSubstring.go
@@ -1,37 +1,20 @@
 package problem0003
 
 func lengthOfLongestSubstring(s string) int {
-	if len(s) < 2 {
-		return len(s)
-	}
-
-	start := 0
-	curr := 0
-	m := 0
+	start, maxLen := 0, 0
 	set := make(map[byte]struct{})
 
-	for curr < len(s) {
-		l := s[curr]
-		_, ok := set[l]
-		if ok {
+	for curr := 0; curr < len(s); curr++ {
+		for _, ok := set[s[curr]]; ok; _, ok = set[s[curr]] {
+			delete(set, s[start])
 			start++
-			curr = start
-			c := len(set)
-			if c > m {
-				m = c
-			}
-			clear(set)
-			continue
 		}
-
-		set[l] = struct{}{}
-		curr++
+		set[s[curr]] = struct{}{}
+		curLen := curr - start + 1
+		if curLen > maxLen {
+			maxLen = curLen
+		}
 	}
 
-	c := len(set)
-	if c > m {
-		m = c
-	}
-
-	return m
+	return maxLen
 }
