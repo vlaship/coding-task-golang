ca02b1b1cdef34301e232b1a2dbb9ae2c40d4e4f
vlaship <vlaship@outlook.com>
2024-11-29 09:56:22 -0500

    switch

--- leetcode/easy/problem0412/fizzBuzz.go
+++ leetcode/easy/problem0412/fizzBuzz.go
@@ -4,18 +4,16 @@ import "strconv"
 
 func fizzBuzz(n int) []string {
 	res := make([]string, n)
-	for i := 1; i < n+1; i++ {
-		r := ""
-		if i%3 == 0 {
-			r += "Fizz"
-		}
-		if i%5 == 0 {
-			r += "Buzz"
-		}
-		if r == "" {
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%15 == 0:
+			res[i-1] = "FizzBuzz"
+		case i%3 == 0:
+			res[i-1] = "Fizz"
+		case i%5 == 0:
+			res[i-1] = "Buzz"
+		default:
 			res[i-1] = strconv.Itoa(i)
-		} else {
-			res[i-1] = r
 		}
 	}
 	return res
