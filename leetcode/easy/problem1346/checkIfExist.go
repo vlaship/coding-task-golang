cc3164ec8e26283d011e4b8f11c4f17f1a4e013a
vlaship <vlaship@outlook.com>
2024-12-01 00:06:46 -0500

    Add solution and tests for LeetCode problem 1346: Check If N and Its Double Exist
    - Implement `checkIfExist` function to determine if there exist two indices `i` and `j` such that `arr[i] == 2 * arr[j]`.
    - Create README with problem description and examples.
    - Add unit tests for various cases using the `testing` package.

---
+++ leetcode/easy/problem1346/checkIfExist.go
@@ -0,0 +1,20 @@
+package problem1346
+
+func checkIfExist(arr []int) bool {
+
+	m := make(map[int]int)
+
+	for i, v := range arr {
+		if j, ok := m[v*2]; ok && i != j {
+			return true
+		}
+		if v%2 == 0 {
+			if j, ok := m[v/2]; ok && i != j {
+				return true
+			}
+		}
+		m[v] = i
+	}
+
+	return false
+}
